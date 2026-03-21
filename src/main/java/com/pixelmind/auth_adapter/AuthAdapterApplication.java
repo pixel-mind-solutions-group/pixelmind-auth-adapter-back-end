65cafc64dbecee6b08e5ae550707547fd8d88abd
Maleesha Sandakalum <maleeshaGitHub0000@gmail.com>
2026-03-21 15:30:21 +0530

    initial project setup

---
+++ src/main/java/com/pixelmind/auth_adapter/AuthAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.pixelmind.auth_adapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthAdapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthAdapterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pixelmind/auth_adapter/AuthAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pixelmind.auth_adapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthAdapterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
