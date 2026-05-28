e7a8a2c3c6cc4b43412de978ccafc3339eedcbca
Maleesha Sandakalum <maleeshaGitHub0000@gmail.com>
2026-05-28 08:18:53 +0530

    inital work

--- src/main/java/com/pixelmind/auth_adapter/service/ApplicationService.java
+++
@@ -1,9 +0,0 @@
-package com.pixelmind.auth_adapter.service;
-
-import com.pixelmind.auth_adapter.dto.CommonResponseDTO;
-import com.pixelmind.auth_adapter.dto.application.ApplicationRequestDTO;
-
-public interface ApplicationService {
-
-    CommonResponseDTO createOrModify(String realmName, ApplicationRequestDTO applicationRequest);
-}
--- src/main/java/com/pixelmind/auth_adapter/util/CommonValidation.java
+++
@@ -1,9 +0,0 @@
-package com.pixelmind.auth_adapter.util;
-
-public class CommonValidation {
-
-    public static boolean stringNullValidation(String inputString) {
-        return inputString == null || inputString.isEmpty();
-    }
-
-}
